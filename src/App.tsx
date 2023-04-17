b15bf92783280c032e794546402ec95210328113
RobinCar <32109406+RobinCar@users.noreply.github.com>
2023-04-17 17:50:55 +0200

    feat: dojo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,59 @@
 import logo from './assets/logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { fetchTracks } from './lib/fetchTracks';
+import { SavedTrack } from 'spotify-types';
+import { ChoiceButton } from './components/ChoiceButton';
+import swal from 'sweetalert';
+
+const pickRandomTrack = (tracks: SavedTrack[]) => {
+  return tracks[Math.floor(Math.random() * tracks.length)]!;
+};
+
+const shuffleArray = (tracks: SavedTrack[]) => {
+  return tracks.sort(() => Math.random() - 0.5);
+};
 
 const App = () => {
+  const {
+    data: tracks,
+    isLoading,
+    isSuccess,
+  } = useQuery({
+    queryKey: ['tracks'],
+    queryFn: fetchTracks,
+  });
+
+  const [currentTrack, setCurrentTrack] = useState<SavedTrack | undefined>(
+    undefined,
+  );
+  const [trackChoices, setTrackChoices] = useState<SavedTrack[]>([]);
+
+  const selectNewTrack = () => {
+    if (!tracks) {
+      return;
+    }
+    const rightTrack = pickRandomTrack(tracks);
+    setCurrentTrack(rightTrack);
+
+    const wrongTracks = [pickRandomTrack(tracks), pickRandomTrack(tracks)];
+    const choices = shuffleArray([rightTrack, ...wrongTracks]);
+    setTrackChoices(choices);
+  };
+
+  useEffect(() => {
+    selectNewTrack();
+  }, [tracks]);
+
+  const checkAnswer = (track: SavedTrack) => {
+    if (track.track?.id == currentTrack?.track?.id) {
+      swal('Bravo !', "C'est la bonne réponse", 'success').then(selectNewTrack);
+    } else {
+      swal('Dommage !', "Ce n'est pas la bonne réponse", 'error');
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +61,28 @@ const App = () => {
         <h1 className="App-title">Bienvenue sur le blind test de Robin</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        {isLoading || !isSuccess ? (
+          'Loading...'
+        ) : (
+          <div>
+            <div>
+              <audio
+                src={currentTrack?.track?.preview_url ?? ''}
+                controls
+                autoPlay
+              />
+            </div>
+          </div>
+        )}
       </div>
-      <div className="App-buttons"></div>
+      <div className="App-buttons">
+        {trackChoices.map(track => (
+          <ChoiceButton
+            track={track.track}
+            onClick={() => checkAnswer(track)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/lib/fetchTracks.ts
+++ src/lib/fetchTracks.ts
@@ -1,5 +1,19 @@
-const apiToken: string = '';
+import { SavedTrack } from 'spotify-types';
 
-export const fetchTracks = async () => {
-  // This function does nothing, yet
+const apiToken =
+  'BQAeZwNAXy3QEHBPkcKDtaO26SbhtbsNTlkQORiJdsqmqIYiEJMSuWw9_FXB2MTEOaGGORBD2ZjYiz5LfiZKnb-feu_qN_Kt7rxaIqXK0v4dtlegX13bcVq3dhd6u5WboBRVG6zNc6cLZ5OChKH4CAQm5zzDAlkXNQiglsSFkpHBIvUSmwlGScVlJ19HBLXNS6W92jY-p2PNiQjvznOdYtu9wgTqb09DsYxRWWm-AqfDWlj14Lz6A-5YMa94DfiBlbe9LG7-zFmIY_T3hPzJugS1xfuQ_dhxCk5zDJCFntAr5o_lTUrs_xwgQwGeJxc-29vzXi-FN6XIpkiTJQKBRXDA_OM';
+
+export const fetchTracks = async (): Promise<SavedTrack[]> => {
+  const response = await fetch('https://api.spotify.com/v1/me/tracks', {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + apiToken,
+    },
+  });
+  if (!response.ok) {
+    throw new Error(`Fetching tracks failed with status ${response.status}`);
+  }
+  const data = (await response.json()) as { items: SavedTrack[] };
+
+  return data.items;
 };
